c2d4356a255862ab1216144c8f6eb7294bc12c5d
fl76at <frankie_lamar1@homedepot.com>
2018-02-08 00:20:05 -0500

    updated travis config

--- hearsay/hearsay.py
+++ hearsay/hearsay.py
@@ -1,4 +1,5 @@
 from giphypop import translate
+import requests
 
 def get_cat_gif():
     """Return Cat Gif URL"""
@@ -8,10 +9,10 @@ def get_cat_gif():
 def get_cat_fact():
     """Retuns Cat Fact JSON"""
     cat_request = requests.get('https://catfact.ninja/fact')
-    return r.text
+    return cat_request.text
 
 if __name__ == '__main__':
-    cat_gif = get_cat_gif()
-    cat_fact = get_cat_fact()
-    print(cat_gif)
-    print(fact_fact)
+    gif = get_cat_gif()
+    fact = get_cat_fact()
+    print(gif)
+    print(fact)
