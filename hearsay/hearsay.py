c31337605823c759f5ef7065a628986d2731e70d
fl76at <frankie_lamar1@homedepot.com>
2018-02-07 23:08:55 -0500

    added travis config

---
+++ hearsay/hearsay.py
@@ -0,0 +1,17 @@
+from giphypop import translate
+from hearsay import hearsay
+
+
+def get_cat_gif():
+    cat_gif = translate('cat')
+    return cat_gif
+
+def get_cat_fact():
+    r = requests.get('https://catfact.ninja/fact')
+    return r.text
+
+if __name__ == '__main__':
+    cat = get_cat_gif()
+    fact = get_cat_fact()
+    print(cat)
+    print(fact)
---
+++ test/test_hearsay.py
@@ -0,0 +1,7 @@
+import pytest
+from hearsay import hearsay
+
+
+def test_get_cat_gif():
+    cat_gif = hearsay.get_cat_gif()
+    assert True
